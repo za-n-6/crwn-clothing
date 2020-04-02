d20733f0fa50fc8911efcd5f79d2ade5f08bd3f1
za-n-6 <zara.nasim96@gmail.com>
2020-04-02 20:55:14 +0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import FormInput from '../form-input/form-input.component';
 
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth ,  signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 
@@ -24,10 +24,18 @@ class SignIn extends React.Component {
 
         }
     }
-        handleSubmit =event => {
+        handleSubmit = async event => {
             event.preventDefault();
 
-            this.setState({ email: '', password: '' })
+            const { email, password } = this.state;
+            try {
+                await auth.signInWithEmailAndPassword(email, password);
+                this.setState({ email: '', password: '' });
+              }  catch (error) {
+                  console.log(error);
+              }
+
+            
         }
 
 
