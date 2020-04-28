320cf3234859934f7943625d79aff8f803849e7d
za-n-6 <zara.nasim96@gmail.com>
2020-04-28 23:11:45 +0500

    removing redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,40 @@
-import { createStore, applyMiddleware } from "redux";
+// import { createStore, applyMiddleware } from "redux";
 
-import logger from "redux-logger";
+// import logger from "redux-logger";
+// import { persistStore } from "redux-persist";
+
+// import rootReducer from "./root-reducer";
+
+// const middlewares = [];
+
+// if ((process.env.NODE_ENV = "development")) {
+//   middlewares.push(logger);
+// }
+
+// //const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// //we are going to export our store out.
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// //Also going to export a persistor which is going to be the persisted version of our store.
+// export const persistor = persistStore(store);
+
+// export default { store, persistor };
+
+import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const middlewares = [logger];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-//const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-//we are going to export our store out.
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-//Also going to export a persistor which is going to be the persisted version of our store.
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+export default { store, persistStore };
